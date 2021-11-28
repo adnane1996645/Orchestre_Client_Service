3b799c99f1cd31af4af68cc4a48353145577ed21
Esteban Mauricio <theredcow2@gmail.com>
2021-11-28 11:56:34 +0100

    compilation sans warning avec memory.h

--- ORCHESTRE/orchestre.c
+++ ORCHESTRE/orchestre.c
@@ -28,44 +28,62 @@ int main(int argc, char * argv[])
     usage(argv[0], "nombre paramètres incorrect");
   
   bool fin = false;
-
+  int *fd;
+  int service;
+  bool serv_fini[3];
+  
   // lecture du fichier de configuration
   config_init(argv[1]);
   
   // Pour la communication avec les clients
   // - création de 2 tubes nommés pour converser avec les clients
-  // - création d'un sémaphore pour que deux clients ne
-  //   ne communiquent pas en même temps avec l'orchestre
+  // - création d'un sémaphore pour que deux clients ne communiquent pas en même temps avec l'orchestre
   creat_named_pipe();
   int semid = creat_mutex();
   
   // lancement des services, avec pour chaque service :
   // - création d'un tube anonyme pour converser (orchestre vers service)
-  // - un sémaphore pour que le service préviene l'orchestre de la
-  //   fin d'un traitement
-  // - création de deux tubes nommés (pour chaque service) pour les
-  //   communications entre les clients et les services
+  // - un sémaphore pour que le service préviene l'orchestre de la fin d'un traitement
+  // - création de deux tubes nommés (pour chaque service) pour les communications entre les clients et les services
   
   
   while (! fin)
     {
       // ouverture ici des tubes nommés avec un client
-      fdOrchestreToClient = open(argv[3], O_WRONLY, 0644);//ouverture des deux tubes nommés avec le client
-      fdClientToOrchestre = open(argv[4], O_RDONLY, 0644);
+      fd = open_pipes_o(); //fd[0]->lecture  /  fd[1]->ecriture
+      
       // attente d'une demande de service du client
+      service = rcv_request(fd[0]);
       
       // détecter la fin des traitements lancés précédemment via
       // les sémaphores dédiés (attention on n'attend pas la
       // fin des traitement, on note juste ceux qui sont finis)
       
+      /*besoin de faire une fonction dans orchestre_service, i.e: bool * getserv_fin(int service); */
+      if(semctl(semid[service], 0, GETVAL, 0) == 0)
+	serv_fini[service] = true;
+      else serv_fini[service] = false;
+
+      
       // analyse de la demande du client
       // si ordre de fin
       //     envoi au client d'un code d'acceptation (via le tube nommé)
       //     marquer le booléen de fin de la boucle
+      if(service == -1){
+	send_reply(fd[1], true);
+	fin = true;
+      }
+	
       // sinon si service non ouvert
       //     envoi au client d'un code d'erreur (via le tube nommé)
-      // sinon si service déjà en cours de traitement
+      else if(config_isServiceOpen(service) == false)
+	send_reply(fd[1], false);
+      
+      // sinon si service est déjà en cours de traitement
       //     envoi au client d'un code d'erreur (via le tube nommé)
+      else if(serv_fini == false)
+	send_reply(fd[1], false);
+      
       // sinon
       //     envoi au client d'un code d'acceptation (via le tube nommé)
       //     génération d'un mot de passe
@@ -76,7 +94,9 @@ int main(int argc, char * argv[])
       // finsi
       
       // attente d'un accusé de réception du client
+      
       // fermer les tubes vers le client
+
       
       // il peut y avoir un problème si l'orchestre revient en haut de la
       // boucle avant que le client ait eu le temps de fermer les tubes
