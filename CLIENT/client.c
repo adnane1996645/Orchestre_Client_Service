5a2a2588d3d9e8fed0547c379a8fd6a5f4dbdc55
Esteban Mauricio <theredcow2@gmail.com>
2021-11-30 19:53:10 +0100

    client.c terminé...manque juste une condition a un if

--- CLIENT/client.c
+++ CLIENT/client.c
@@ -91,26 +91,57 @@ int main(int argc, char * argv[])
     send_adc(fd[1]);
     
     // fermeture des tubes avec l'orchestre
+    close_pipes(fd);
+    
     // sortie de la section critique
+    v_mutex(semid);
+    
     //
     // si pas d'erreur et service normal
+    ///////****je vois pas à quoi çà correspond****////////
+    if(/*condition?? */){  
+    
     //     ouverture des tubes avec le service
+      int fdW = openTubeWrite(getPipe(com, 1), "erreur d'ouverture en écriture du tube entre le client et les service");
+      int fdR = openTubeRead(getPipe(com, 2), "erreur d'ouverture en lecture du tube entre le client et les service");
+      
     //     envoi du mot de passe au service
+      sendPWD(fd[1], getPwd(com));
+      
     //     attente de l'accusé de réception du service
+      getACR(fd[0]);
     //     si mot de passe non accepté
     //         message d'erreur
+      if(getReponsePWD(fd[0]) == WRONG_PWD)
+	printf("le mot de passe n'a pas été accepté\n");
     //     sinon
     //         appel de la fonction de communication avec le service :
     //             une fct par service selon numService :
     //                    . client_somme
     //                 ou . client_compression
     //                 ou . client_maximum
+      else {
+	if(numService == 0)
+	  client_somme(fd[0], fd[1], argc, argv[]);
+	else if(numService == 1)
+	  client_compression(fd[0], fd[1], argc, argv[]);
+	else if(numService == 2)
+	  client_maximum(fd[0], fd[1], argc, argv[]);
+	
     //         envoi d'un accusé de réception au service
+	sendACR(fd[1]);
+	
     //     finsi
+      }
+      
     //     fermeture des tubes avec le service
+      closeTubeWrite(fdW, "erreur de fermeture en écriture du tube entre le client et les service");
+      closeTubeRead(fdR, "erreur de fermeture en lecture du tube entre le client et les service");
     // finsi
-
+    }
+    
     // libération éventuelle de ressources
+    
     
     return EXIT_SUCCESS;
 }
