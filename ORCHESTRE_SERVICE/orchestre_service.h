100314c87dc61c340dc7b6d703e9f62ccd37d5e0
adnane1996645 <adnane.laanani@gmail.com>
2021-12-03 22:50:50 +0100

    Qlq retouche sur orchestre_service.h

--- ORCHESTRE_SERVICE/orchestre_service.h
+++ ORCHESTRE_SERVICE/orchestre_service.h
@@ -10,10 +10,7 @@
 #define ORCHESTRE_SERVICE "orchestre_service.h"
 
 typedef struct OrderP * Order;
-int mysemget();
-void mysem_descre(int semId); // desincremente la valeur du semaphore par 1
-void mysem_incre(int semId); // incremente la valeur du semaphore par 1
-void mysem_destroy(int semId); // détruire le sémaphore
+
 Order init_Order(bool isok, const int motpasse);
 bool isItOk(Order self);
 int getMotpasse(Order self);
@@ -21,5 +18,10 @@ void destroy_Order(Order * pself);
 void OrderOrchestreToService(int fdWrite,  Order order);
 Order getOrderFromOrchestre(int fdRead);
 
+int mysemget(int pojid);
+void mysem_descre(int semId); // desincremente la valeur du semaphore par 1
+void mysem_incre(int semId); // incremente la valeur du semaphore par 1
+void mysem_destroy(int semId); // détruire le sémaphore
+void mysem_attente(int semId);
 
 #endif
